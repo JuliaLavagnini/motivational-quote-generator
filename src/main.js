83c45b3a6e015912ce14d6240db61e7c0c7d5e83
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2024-01-04 01:25:10 +0000

    Add functionality

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
-//Motivational Quote Generator:
-//Create a tool that generates motivational quotes or affirmations to inspire users. 
-//Users can specify the type of motivation they need.
+const btn = document.querySelector(".submit-btn");
 
-const key = "568346b6b43fda3549abo850e089d8at";
-const context = "Please be kind and creative";
-let category = "be yourself"
-let prompt = `Write a motivational quote about ${category}`;
-const apUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
-
-function showAnswer(response) {
-   console.log(response.data.answer);
+function showQuote(response) {
+   btn.addEventListener("click", () => {
+      new Typewriter("#quoteGenerated", {
+         strings: response.data.answer,
+         autoStart: true,
+         delay: 30,
+      });
+   });
 }
 
-axios.get(apUrl).then(showAnswer);
+function generateQuote(event){
+   event.preventDefault();
+   
+   let category = document.querySelector("#userPrompt");
+   const key = "568346b6b43fda3549abo850e089d8at";
+   const context = "Please be kind and creative. Make sure to follow the user instructions.";
+   let prompt = `Write a motivational quote about ${category}`;
+   const url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+   
+   console.log(prompt)
+   axios.get(url).then(showQuote);
+}
+
+const quoteFormElement = document.querySelector(
+   "#quoteGeneratorForm"
+   );
+   quoteFormElement.addEventListener("submit", generateQuote);
