469a39a86b0fff8d89b0e1c246bc238707f91bcd
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2024-01-09 13:06:41 +0000

    Add functionality with js script

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-const btn = document.querySelector(".submit-btn");
-
 function showQuote(response) {
-   btn.addEventListener("click", () => {
-      new Typewriter("#quoteGenerated", {
-         strings: response.data.answer,
-         autoStart: true,
-         delay: 30,
-      });
+   console.log('quote generated');
+   new Typewriter("#quoteGenerated", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 30,
+      cursor: "",
    });
 }
 
@@ -14,16 +12,18 @@ function generateQuote(event){
    event.preventDefault();
    
    let category = document.querySelector("#userPrompt");
-   const key = "568346b6b43fda3549abo850e089d8at";
-   const context = "Please be kind and creative. Make sure to follow the user instructions.";
-   let prompt = `Write a motivational quote about ${category}`;
-   const url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+   let key = "568346b6b43fda3549abo850e089d8at";
+   let context =
+     "Your friend needs a motivation to feel better. Your mission is to create a 2 lines motivational quote in basic HTML. Make sure to follow the user instructions. Sign the quote with 'Stay Motivated ❤️' inside a <strong> element at the end of the quote and NOT at the beginning. The quote should use lighter font-weight and the sign. Everything should align to the left. ";
+   let prompt = `User instruction: Write a motivational quote about ${category.value}`;
+   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+   console.log("generating quote");
+   console.log(`${prompt}`);
+   console.log(`${context}`);
    
-   console.log(prompt)
    axios.get(url).then(showQuote);
 }
 
-const quoteFormElement = document.querySelector(
-   "#quoteGeneratorForm"
-   );
-   quoteFormElement.addEventListener("submit", generateQuote);
+const quoteFormElement = document.querySelector("#quoteGeneratorForm");
+quoteFormElement.addEventListener("submit", generateQuote);
